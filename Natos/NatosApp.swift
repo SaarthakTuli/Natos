c7dbbf3179f33a76b824a9774506fbb9b782abce
SaarthakTuli <saarthaktuli@gmail.com>
2022-12-28 10:39:55 +0530

    Initial Commit

---
+++ Natos/NatosApp.swift
@@ -0,0 +1,17 @@
+//
+//  NatosApp.swift
+//  Natos
+//
+//  Created by Saarthak Tuli on 28/12/22.
+//
+
+import SwiftUI
+
+@main
+struct NatosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
